19f9a457be5b3f0f2dd9658a1b6b95d3448c3acd
nilesh9758 <153944540+nilesh9758@users.noreply.github.com>
2025-06-25 12:14:30 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,22 @@
-// /pages/_app.js
+// pages/_app.js
+import Footer from "@/components/ui/Footer";
+import Navbar from "@/components/ui/Navbar";
+import WatermarkBackground from "@/components/ui/WatermarkBackground";
 import { PointsProvider } from "@/context/PointContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <PointsProvider>
-      <Component {...pageProps} />
-    </PointsProvider>
+    <WatermarkBackground>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-1">
+          <PointsProvider>
+            <Component {...pageProps} />
+          </PointsProvider>
+        </main>
+        <Footer />
+      </div>
+    </WatermarkBackground>
   );
 }
