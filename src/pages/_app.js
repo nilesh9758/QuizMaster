57d8c68635fcbe6f0616c25956e19e16f4a7d4dc
nilesh9758 <153944540+nilesh9758@users.noreply.github.com>
2025-06-25 12:13:03 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+// /pages/_app.js
+import { PointsProvider } from "@/context/PointContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <PointsProvider>
+      <Component {...pageProps} />
+    </PointsProvider>
+  );
 }
